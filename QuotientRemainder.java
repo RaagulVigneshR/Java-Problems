f11bac80a92d1e302c5e40b49e303717a394241a
Rohithv17 <148313542+Rohithv17@users.noreply.github.com>
2024-01-18 00:34:55 +0530

    Add files via upload

--- QuotientRemainder.java
+++ QuotientRemainder.java
@@ -1,19 +1,24 @@
+
 import java.awt.*;
+
 class program {
-    void swap(int a,int b){
+    void Quotient(int dividend,int divisor){
+        int quotient=dividend/divisor;
+        System.out.println("Quotient is:"+quotient);
 
-       int temp=a;
-       b=a;
-       a=temp;
-        System.out.println("swapped values are:"+a+b);
-    }                                 
+    }
+    void Remainder(int dividend,int divisor){
+        int remainder=dividend%divisor;
+        System.out.println("Remainder is:"+remainder);
 
-}
+}}
 
 
 class  Main1{
     public static void main(String[] args) {
-        int a,int b;
+        int dividend, divisor;
         program p=new program();
-        p.swap(6,5);
-}}
+        p.Quotient(6,3);
+        p.Remainder(6,3);
+
+    }}
