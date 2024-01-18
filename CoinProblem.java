fcdc99d2b078ad865cc7cd05bf74d3752d72cefb
Rohithv17 <148313542+Rohithv17@users.noreply.github.com>
2024-01-18 12:22:56 +0530

    Add files via upload

---
+++ CoinProblem.java
@@ -0,0 +1,31 @@
+import java.lang.Math;
+import java.util.Scanner;
+class program{
+
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter number of times to flip the coin");
+        int flips= input.nextInt();
+
+
+
+        int headcount=0,tailcount=0;
+        for(int i=1;i<=flips;i++) {
+            double  rand= Math.random();
+            if (rand >= 0 && rand < 0.5) {
+                tailcount++;
+
+            } else {
+                headcount++;
+            }
+        }
+        System.out.println("Headcount is:"+headcount);
+        System.out.println("Tailcount is:"+tailcount);
+        double headPercentage = ((double) headcount / flips) * 100;
+        System.out.println("Heads percentage=" + headPercentage);
+        double tailPercentage = 100-(headPercentage);
+        System.out.println("Tails percentage=" + tailPercentage);
+
+
+    }}
+
